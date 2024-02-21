67f6a5a8170d776c610e6bea14ebda3e88b71cd1
Wei <lydia9017@gmail.com>
2024-02-21 13:06:00 +0800

    Update - adjust footer and playing modal

--- src/components/Modal/PlayingModal/PlayingModal.js
+++ src/components/Modal/PlayingModal/PlayingModal.js
@@ -8,27 +8,36 @@ import {
 } from "react-icons/io5";
 import { LiaRandomSolid } from "react-icons/lia";
 import { GrPowerCycle } from "react-icons/gr";
-import { useEffect, useState, useRef } from "react";
 import { IoMdVolumeOff } from "react-icons/io";
 import "./PlayingModal.scss";
 import { useSelector, useDispatch } from "react-redux";
-import { togglePlayPause, setCurrentSong } from "../../store/musicSlice";
+import { togglePlayPause } from "../../store/musicSlice";
 import YouTube from "react-youtube";
 import he from "he";
 import Modal from "react-modal";
 import { PiMusicNotesFill } from "react-icons/pi";
 
-const PlayingModal = ({ isOpen, close }) => {
-  const [volume, setVolume] = useState(100);
-  const [volumeOn, setVolumeOn] = useState(true);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [lengthofsong, setLengthofsong] = useState(0);
-  const playerRef = useRef(null);
+const PlayingModal = ({
+  isOpen,
+  close,
+  lastSong,
+  nextSong,
+  currSong,
+  playerRef,
+  timeToSeconds,
+  handleSchedule,
+  currentTime,
+  setCurrentTime,
+  lengthofsong,
+  formatStart,
+  volume,
+  volumeOn,
+  setVideoVolume,
+  volumeOnOrOff,
+  isPlaying,
+}) => {
   const dispatch = useDispatch();
-  const isPlaying = useSelector((state) => state.music.isPlaying);
   const videoId = useSelector((state) => state.music.currentSong.id);
-  const currSong = useSelector((state) => state.music.currentSong);
-  const currentPlaylist = useSelector((state) => state.music.currentPlaylist);
 
   const customModalStyle = {
     overlay: {
@@ -52,137 +61,6 @@ const PlayingModal = ({ isOpen, close }) => {
     playerVars: {
       autoplay: 1,
     },
-  };
-
-  //play and pause
-  useEffect(() => {
-    if (playerRef.current) {
-      const player = playerRef.current.getInternalPlayer();
-      if (player) {
-        if (isPlaying) {
-          if (player.getPlayerState() !== 1) {
-            player.playVideo();
-          }
-        } else {
-          player.pauseVideo();
-        }
-      }
-    }
-  }, [isPlaying]);
-
-  const volumeOnOrOff = (condition) => {
-    setVolumeOn(condition);
-    if (condition) {
-      setVolume(30);
-    } else {
-      setVolume(0);
-    }
-  };
-
-  const setVideoVolume = (newVolume) => {
-    if (playerRef.current && playerRef.current.internalPlayer) {
-      playerRef.current.internalPlayer.setVolume(newVolume);
-      setVolume(newVolume);
-    }
-    if (newVolume === 0) {
-      setVolumeOn(false);
-    } else {
-      setVolumeOn(true);
-    }
-  };
-
-  const handleSchedule = (e) => {
-    const time = parseFloat(e.target.value);
-    setCurrentTime(time);
-    if (playerRef.current && playerRef.current.internalPlayer) {
-      playerRef.current.internalPlayer.seekTo(time, true);
-    }
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (isPlaying && playerRef.current && playerRef.current.internalPlayer) {
-        playerRef.current.internalPlayer
-          .getCurrentTime()
-          .then((currentTime) => {
-            setCurrentTime(Math.floor(currentTime));
-          });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [currentTime, isPlaying]);
-
-  const timeToSeconds = (timeString) => {
-    if (timeString) {
-      const timeArray = timeString.split(":").map(Number);
-      let totalSeconds = 0;
-      if (timeArray.length === 3) {
-        totalSeconds += timeArray[0] * 3600;
-        totalSeconds += timeArray[1] * 60;
-        totalSeconds += timeArray[2];
-      } else if (timeArray.length === 2) {
-        totalSeconds += timeArray[0] * 60;
-        totalSeconds += timeArray[1];
-      }
-      return totalSeconds;
-    }
-  };
-
-  const formatTime = (seconds) => {
-    let hours = Math.floor(seconds / 3600);
-    let minutes = Math.floor((seconds % 3600) / 60);
-    let remainingSeconds = Math.floor(seconds % 60);
-
-    hours = hours < 10 ? "0" + hours : hours;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    remainingSeconds =
-      remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
-
-    let timeString = hours + ":" + minutes + ":" + remainingSeconds;
-
-    if (hours === "00") {
-      timeString = minutes + ":" + remainingSeconds;
-    }
-
-    return timeString;
-  };
-
-  useEffect(() => {
-    if (currSong.duration) {
-      const percent =
-        (Math.round((currentTime / timeToSeconds(currSong.duration)) * 100) /
-          100) *
-        100;
-      setLengthofsong(percent);
-    }
-  }, [currSong, currentTime]);
-
-  const nextSong = () => {
-    if (currentPlaylist !== null) {
-      const songlist = currentPlaylist.songs;
-      const index = songlist.findIndex((item) => item.id === currSong.id);
-      if (index + 2 < songlist.length) {
-        dispatch(setCurrentSong(songlist[index + 1]));
-      } else {
-        dispatch(setCurrentSong(songlist[0]));
-      }
-    } else {
-      return false;
-    }
-  };
-
-  const lastSong = () => {
-    if (currentPlaylist !== null) {
-      const songlist = currentPlaylist.songs;
-      const index = songlist.findIndex((item) => item.id === currSong.id);
-      if (index === 0) {
-        dispatch(setCurrentSong(songlist[songlist.length]));
-      } else {
-        dispatch(setCurrentSong(songlist[index - 1]));
-      }
-    } else {
-      return false;
-    }
   };
 
   return (
@@ -216,15 +94,15 @@ const PlayingModal = ({ isOpen, close }) => {
               type="range"
               value={Math.floor(currentTime)}
               min={0}
-              max={timeToSeconds(currSong.duration)}
-              onChange={(e) => handleSchedule(e)}
+              max={timeToSeconds}
+              onChange={handleSchedule}
             />
             <p
               className="bg-white h-1.5 relative rounded -top-2.5"
               style={{ width: `${lengthofsong}%` }}
             ></p>
             <div className="flex justify-between text-xs">
-              <span>{formatTime(currentTime)}</span>
+              <span>{formatStart}</span>
               <span>{currSong.duration}</span>
             </div>
             <div className="flex justify-center items-center mt-2 mb-6">
@@ -232,7 +110,7 @@ const PlayingModal = ({ isOpen, close }) => {
               <div className="flex justify-center ">
                 <IoPlayBack
                   className="w-8 h-8 text-white mx-2 drop-shadow-lg cursor-pointer"
-                  onClick={() => lastSong()}
+                  onClick={lastSong}
                 />
                 {!isPlaying ? (
                   <IoPlay
@@ -247,7 +125,7 @@ const PlayingModal = ({ isOpen, close }) => {
                 )}
                 <IoPlayForward
                   className="w-8 h-8 text-white mx-2 drop-shadow-lg cursor-pointer active:drop-shadow-none"
-                  onClick={() => nextSong()}
+                  onClick={nextSong}
                 />
               </div>
               <GrPowerCycle className="w-5 h-5 mx-4 cursor-pointer" />
--- src/components/Object/Footer/index.js
+++ src/components/Object/Footer/index.js
@@ -11,30 +11,185 @@ import { useSelector, useDispatch } from "react-redux";
 import { togglePlayPause } from "../../store/musicSlice";
 import { IoMdVolumeOff } from "react-icons/io";
 import PlayingModal from "../../Modal/PlayingModal/PlayingModal";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import he from "he";
 import { PiMusicNotesFill } from "react-icons/pi";
 import classNames from "classnames";
 import "./Footer.scss";
+import { setCurrentSong } from "../../store/musicSlice";
 
 const Footer = () => {
   const isPlaying = useSelector((state) => state.music.isPlaying);
-  const currentSong = useSelector((state) => state.music.currentSong);
   const dispatch = useDispatch();
   const [openPlayModal, setOpenPlayModal] = useState(false);
+  // const [volumeOn, setVolumeOn] = useState(true);
+  const currSong = useSelector((state) => state.music.currentSong);
+  const currentPlaylist = useSelector((state) => state.music.currentPlaylist);
+  const playerRef = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [lengthofsong, setLengthofsong] = useState(0);
+  const [volume, setVolume] = useState(100);
   const [volumeOn, setVolumeOn] = useState(true);
 
-  const volumeOnOrOff = (condition) => {
-    setVolumeOn(condition);
-  };
+  // const volumeOnOrOff = (condition) => {
+  //   setVolumeOn(condition);
+  // };
 
   const toPlaying = (condition) => {
     setOpenPlayModal(condition);
   };
 
+  const volumeOnOrOff = (condition) => {
+    setVolumeOn(condition);
+    if (condition) {
+      setVolume(30);
+    } else {
+      setVolume(0);
+    }
+  };
+
+  const setVideoVolume = (newVolume) => {
+    if (playerRef.current && playerRef.current.internalPlayer) {
+      playerRef.current.internalPlayer.setVolume(newVolume);
+      setVolume(newVolume);
+    }
+    if (newVolume === 0) {
+      setVolumeOn(false);
+    } else {
+      setVolumeOn(true);
+    }
+  };
+
+  const nextSong = () => {
+    if (currentPlaylist !== null) {
+      const songlist = currentPlaylist.songs;
+      const index = songlist.findIndex((item) => item.id === currSong.id);
+      if (index + 2 < songlist.length) {
+        dispatch(setCurrentSong(songlist[index + 1]));
+      } else {
+        dispatch(setCurrentSong(songlist[0]));
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const lastSong = () => {
+    if (currentPlaylist !== null) {
+      const songlist = currentPlaylist.songs;
+      const index = songlist.findIndex((item) => item.id === currSong.id);
+      if (index === 0) {
+        dispatch(setCurrentSong(songlist[songlist.length]));
+      } else {
+        dispatch(setCurrentSong(songlist[index - 1]));
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const timeToSeconds = (timeString) => {
+    if (timeString) {
+      const timeArray = timeString.split(":").map(Number);
+      let totalSeconds = 0;
+      if (timeArray.length === 3) {
+        totalSeconds += timeArray[0] * 3600;
+        totalSeconds += timeArray[1] * 60;
+        totalSeconds += timeArray[2];
+      } else if (timeArray.length === 2) {
+        totalSeconds += timeArray[0] * 60;
+        totalSeconds += timeArray[1];
+      }
+      return totalSeconds;
+    }
+  };
+
+  const handleSchedule = (e) => {
+    const time = parseFloat(e.target.value);
+    setCurrentTime(time);
+    if (playerRef.current && playerRef.current.internalPlayer) {
+      playerRef.current.internalPlayer.seekTo(time, true);
+    }
+  };
+
+  useEffect(() => {
+    if (currSong.duration) {
+      const percent =
+        (Math.round((currentTime / timeToSeconds(currSong.duration)) * 100) /
+          100) *
+        100;
+      setLengthofsong(percent);
+    }
+  }, [currSong, currentTime]);
+
+  const formatTime = (seconds) => {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds % 3600) / 60);
+    let remainingSeconds = Math.floor(seconds % 60);
+
+    hours = hours < 10 ? "0" + hours : hours;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    remainingSeconds =
+      remainingSeconds < 10 ? "0" + remainingSeconds : remainingSeconds;
+
+    let timeString = hours + ":" + minutes + ":" + remainingSeconds;
+
+    if (hours === "00") {
+      timeString = minutes + ":" + remainingSeconds;
+    }
+
+    return timeString;
+  };
+
+  useEffect(() => {
+    if (playerRef.current) {
+      const player = playerRef.current.getInternalPlayer();
+      if (player) {
+        if (isPlaying) {
+          if (player.getPlayerState() !== 1) {
+            player.playVideo();
+          }
+        } else {
+          player.pauseVideo();
+        }
+      }
+    }
+  }, [isPlaying]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isPlaying && playerRef.current && playerRef.current.internalPlayer) {
+        playerRef.current.internalPlayer
+          .getCurrentTime()
+          .then((currentTime) => {
+            setCurrentTime(Math.floor(currentTime));
+          });
+      }
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [currentTime, isPlaying]);
+
   return (
     <>
-      <PlayingModal isOpen={openPlayModal} close={() => toPlaying(false)} />
+      <PlayingModal
+        isOpen={openPlayModal}
+        close={() => toPlaying(false)}
+        lastSong={() => lastSong()}
+        currSong={currSong}
+        nextSong={() => nextSong()}
+        playerRef={playerRef}
+        timeToSeconds={timeToSeconds(currSong.duration)}
+        handleSchedule={(e) => handleSchedule(e)}
+        currentTime={currentTime}
+        setCurrentTime={setCurrentTime}
+        lengthofsong={lengthofsong}
+        formatStart={formatTime(currentTime)}
+        volume={volume}
+        volumeOn={volumeOn}
+        setVideoVolume={setVideoVolume}
+        volumeOnOrOff={volumeOnOrOff}
+        isPlaying={isPlaying}
+      />
       <div
         className="fixed bottom-0 flex bg-white h-[70px] cursor-pointer justify-between items-center w-full z-20 p-4 border-t-[1px] border-lightGray"
         onClick={() => toPlaying(true)}
@@ -44,21 +199,19 @@ const Footer = () => {
             className={classNames(
               "bg-black rounded-md min-w-[50px] w-[50px] h-[50px] flex items-center",
               {
-                "bg-white shadow-md justify-center": currentSong === "",
+                "bg-white shadow-md justify-center": currSong === "",
               }
             )}
           >
-            {currentSong !== "" ? (
-              <img alt="thumnail of video" src={currentSong.imgUrl} />
+            {currSong !== "" ? (
+              <img alt="thumnail of video" src={currSong.imgUrl} />
             ) : (
               <PiMusicNotesFill className="w-7 h-7" />
             )}
           </div>
           <div className="flex flex-col text-xs ml-3 font-bold truncate">
-            <span>
-              {currentSong !== "" ? he.decode(currentSong.title) : ""}
-            </span>
-            <span>- {currentSong.channel} -</span>
+            <span>{currSong !== "" ? he.decode(currSong.title) : ""}</span>
+            <span>- {currSong.channel} -</span>
           </div>
         </div>
 
@@ -66,7 +219,10 @@ const Footer = () => {
           <div className="flex justify-center w-[500px] items-center">
             <LiaRandomSolid className="w-4 h-4 mx-4 hover:text-themeBlue" />
             <div className="flex justify-center items-center">
-              <IoPlayBack className="w-6 h-6 mx-2 hover:text-themeBlue" />
+              <IoPlayBack
+                className="w-6 h-6 mx-2 hover:text-themeBlue"
+                onClick={() => lastSong()}
+              />
               {!isPlaying ? (
                 <IoPlay
                   className="w-7 h-7 mx-2 drop-shadow-lg cursor-pointer hover:text-themeBlue"
@@ -78,17 +234,29 @@ const Footer = () => {
                   onClick={() => dispatch(togglePlayPause())}
                 />
               )}
-              <IoPlayForward className="w-6 h-6 mx-2 hover:text-themeBlue" />
+              <IoPlayForward
+                className="w-6 h-6 mx-2 hover:text-themeBlue"
+                onClick={() => nextSong()}
+              />
             </div>
             <GrPowerCycle className="w-4 h-4 mx-4 hover:text-themeBlue" />
           </div>
           <div className="music-control flex relative items-center justify-center text-xs mt-1">
-            <span>00:00</span>
-            <input type="range" value={0} />
+            <span>{formatTime(currentTime)}</span>
+            <input
+              type="range"
+              value={Math.floor(currentTime)}
+              min={0}
+              max={timeToSeconds(currSong.duration)}
+              onChange={(e) => handleSchedule(e)}
+            />
             <div className="absolute w-[80%]">
-              <p className="w-[30%] h-1 bg-grayBg"> </p>
+              <p
+                className="h-1 bg-grayBg"
+                style={{ width: `${lengthofsong}%` }}
+              ></p>
             </div>
-            <span>{currentSong.duration}</span>
+            <span>{currSong.duration}</span>
           </div>
         </div>
         <div className="w-[30%] footer flex justify-end relative items-center">
@@ -103,9 +271,16 @@ const Footer = () => {
               className="mr-2"
             />
           )}
-          <input type="range" value={100} />
+          <input
+            type="range"
+            className="z-5 cursor-pointer"
+            min="0"
+            max="100"
+            value={volume}
+            onChange={(e) => setVideoVolume(parseInt(e.target.value))}
+          />
           <div className="absolute w-[50%]">
-            <p className="w-[30%] h-1 bg-grayBg"> </p>
+            <p className="h-1 bg-grayBg" style={{ width: `${volume}%` }}></p>
           </div>
         </div>
       </div>
